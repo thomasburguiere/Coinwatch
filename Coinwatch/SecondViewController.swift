eb00224657b70f35c321fdac2ffd562e3cadf82f
Thomas Burguiere <Thomas.Burguiere@swisscom.com>
2018-01-22 17:23:18 +0100

    Initial Commit

---
+++ Coinwatch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Coinwatch
+//
+//  Created by Burguiére Thomas, SAS-OXD-EDE-OHS on 22.01.2018.
+//  Copyright © 2018 Burguiére Thomas, SAS-OXD-EDE-OHS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
